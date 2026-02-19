f4b91244c2758d5f6b6594fa9e4d1b403b6d4bf4
4ndr3x-osu <andreap2007@proton.me>
2026-02-19 10:24:58 +0100

    Upload vecchie prove/esercizi
    Caricamento di tutte le prove e gli esercizi effettuati dal 16/02/2026 al 18/02/2026

---
+++ 17-02-2026/Pomeriggio/Esercizi_cicli/Esercizi_cicli.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //-----ESERCIZIO 1
+        bool negativo = false;
+        int n;
+        int somma = 0;
+
+        while (!negativo)
+        {
+            Console.WriteLine("Inserisci un numero positivo: ");
+            n = int.Parse(Console.ReadLine());
+            if(n >= 0)
+            {
+                somma += n;
+            }
+            else
+            {
+                negativo = true;
+                Console.WriteLine($"Fine ciclo. Risultato: {somma}");
+            }
+        }
+
+
+
+        //-----ESERCIZIO 2
+        bool trovato = false;
+        const int SEGRETO = 727;
+        int num;
+
+        while (!trovato)
+        {
+            Console.WriteLine("Inserisci un numero: ");
+            num = int.Parse(Console.ReadLine());
+            if(num != SEGRETO)
+            {
+                if(num > SEGRETO)
+                {
+                    Console.WriteLine("Il numero inserito è maggiore del numero segreto. Ritenta");
+                }
+                else
+                {
+                    Console.WriteLine("Il numero inserito è minore del numero segreto. Ritenta");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Hai indovinato il numero segreto");
+                trovato = true;
+            }
+        }
+
+
+
+        //-----ESERCIZIO 3
+        bool esecuzione = true;
+        string scelta;
+        float saldo = 0.0f;
+        float operazione;
+
+        while (esecuzione)
+        {
+            Console.WriteLine("[1] - Visualizza saldo \n[2] - Deposita denaro \n[3] - Preleva denaro \n[4] - Esci");
+            scelta = Console.ReadLine();
+
+            switch(scelta)
+            {
+                case "1":                                                                       //VISUALIZZA SALDO
+                    Console.WriteLine($"Saldo attuale: {saldo}");
+                    break;
+
+
+
+                case "2":                                                                       //DEPOSITA DENARO
+                    Console.WriteLine("Inserire l'importo da depositare: ");
+                    operazione = float.Parse(Console.ReadLine());
+                    if(operazione < 0)
+                    {
+                        Console.WriteLine("Impossibile depositare una somma inferiore a 0.");
+                    }
+                    else
+                    {
+                        saldo += operazione;
+                        Console.WriteLine($"Deposito effettuato. Saldo attuale: {saldo}");
+                    }
+                    break;
+
+
+                    
+                case "3":                                                                       //PRELEVA DENARO
+                    Console.WriteLine("Inserire l'importo da prelevare: ");
+                    operazione = float.Parse(Console.ReadLine());
+
+                    if(operazione > saldo)
+                    {
+                        Console.WriteLine("Impossibile prelevare una somma maggiore del saldo.");
+                    }
+                    else if(operazione < 0.0f)
+                    {
+                        Console.WriteLine("Impossibile prelevare una somma inferiore a 0.");
+                    }
+                    else
+                    {
+                        saldo -= operazione;
+                        Console.WriteLine($"Prelievo eseguito. Saldo attuale: {saldo}");
+                    }
+                    break;
+
+
+
+                case "4":                                                                        //ESCI
+                    Console.WriteLine($"Arrivederci");
+                    esecuzione = false;
+                    break;
+
+
+
+                default:
+                    Console.WriteLine("Scelta non valida");
+                    break;
+            }
+        }
+    }
+}
---
+++ 18-02-2026/Mattina/Esercizi_for/Esercizi_for.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //-----ESERCIZIO 1
+        Console.WriteLine("Inserisci un numero: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{n} x {i} = {n * i}");
+        }
+
+
+
+        //-----ESERCIZIO 2
+        Console.WriteLine("Quanti numeri vuoi inserire?");
+        int max = int.Parse(Console.ReadLine());
+
+        float somma = 0;
+
+        for(int i = 1; i <= max; i++)
+        {
+            Console.WriteLine($"Inserisci il {i}° numero:");
+            somma += float.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine($"La media dei numeri inseriti è {somma / max}");
+
+
+
+        //-----ESERCIZIO 3
+        int f;
+        
+        do
+        {
+            Console.WriteLine("Inserisci un numero positivo: ");
+            n = int.Parse(Console.ReadLine());
+            f = n;
+
+            if(n < 0)
+            {
+                Console.WriteLine("Errore");
+            }
+        }
+        while(n < 0);
+
+        for(int i = 1; i < n; i++)
+        {
+            Console.WriteLine($"{f} x {n - i}");    //controllo operazione
+            f = f * (n - i);
+        }
+
+        Console.WriteLine($"Il fattoriale di {n} è {f}");
+    }
+}
